0d9de69bd8bf0d7adffabcbcef277ec075ce54ba
Dmytro Chumachenko <dmytro.chumachenko@nixsolutions.com>
2023-03-22 15:50:35 +0100

    asd

--- tg_notification.sh
+++ tg_notification.sh
@@ -2,26 +2,23 @@
 
 TIME="10"
 
-if [ $1 == 'success' ]
+if [[ "$1" == "success" ]]
 then
-  TEXT="Deploy status: ✅%0A%0"
-elif [ $1 == 'failed' ]
+  echo "im here"
+  TEXT="Deploy status: ✅%0A%0A"
+elif [[ "$1" == "failed" ]]
 then
-  TEXT="Deploy status: ❌%0A%0
-  Deploy failed in:%0A"
+  echo "im here"
+  TEXT="Deploy status: ❌%0A%0Deploy failed in:%0A"
 fi
 
-TEXT=TEXT + "Action: $GITHUB_EVENT_NAME%0A
-Repository: $GITHUB_REPOSITORY%0A
-Actor: $GITHUB_ACTOR%0A
-Branch: $GITHUB_HEAD_REF%0A
-URL: $GITHUB_SERVER_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_REPOSITORY%0A
+TEXT="${TEXT}Action: $GITHUB_EVENT_NAME
+Repository: $GITHUB_REPOSITORY
+Actor: $GITHUB_ACTOR
+Branch: $GITHUB_HEAD_REF
+URL: $GITHUB_SERVER_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_REPOSITORY
 "
 
 URL="https://api.telegram.org/bot$TG_BOT_TOKEN/sendMessage"
 
-apt update -y
-apt upgrade curl -y
-apt install curl -y
-
-curl -s --max-time $TIME -d "chat_id=$TG_CHAT_ID&disable_web_page_preview=1&text=$TEXT" $URL
+curl -s --max-time $TIME -d "chat_id=$TG_CHAT_ID&disable_web_page_preview=1&text=$TEXT" "$URL"
