19a9f67111fc0be650810a4fdfb4d1fdb75ff928
Dmytro Chumachenko <dmytro.chumachenko@nixsolutions.com>
2023-03-23 19:07:53 +0100

    add some tests and lynters

--- flaskr/config/development.py
+++ flaskr/config/development.py
@@ -6,9 +6,9 @@ load_dotenv()
 
 DEBUG = True
 SQLALCHEMY_ECHO = False
-SECRET_KEY = os.environ["FLASK_SECRET_KEY"]
-JWT_SECRET_KEY = os.environ["JWT_SECRET_KEY"]
+SECRET_KEY = os.getenv("SECRET_KEY")
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
 SQLALCHEMY_DATABASE_URI = \
-    f"postgresql://{os.environ['POSTGRES_USER']}:{os.environ['POSTGRES_PASSWORD']}@localhost:5432/postgres"
+    f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@localhost:5432/postgres"
 BUNDLE_ERRORS = True
 JSON_SORT_KEYS = False
