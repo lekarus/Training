84d93e59f227ff3613ee7b1db505c9021f57ab15
Dmytro Chumachenko <dmytro.chumachenko@nixsolutions.com>
2023-04-27 16:06:09 +0300

    add swagger

--- flaskr/utils.py
+++ flaskr/utils.py
@@ -5,6 +5,7 @@ from auth.auth import admin_required
 from database import db
 from flask import Blueprint
 from flask_restful import abort, Api, Resource
+from flask_restful_swagger import swagger
 from flask_sqlalchemy.model import Model
 from flask_sqlalchemy.session import Session
 from sqlalchemy.exc import IntegrityError
@@ -13,7 +14,12 @@ from sqlalchemy.exc import IntegrityError
 def create_blueprint_with_api(print_name: str, url_prefix: str = None) -> Tuple[Blueprint, Api]:
     """return blueprint and api instances"""
     blueprint = Blueprint(print_name, __name__, url_prefix=("/" + url_prefix) if url_prefix else None)
-    api = Api(blueprint)
+    api = swagger.docs(
+        Api(blueprint),
+        apiVersion='0.1',
+        api_spec_url="/swagger",
+        description=f"Auto generated API docs by flask-restful-swagger for {print_name}",
+    )
     return blueprint, api
 
 
@@ -72,9 +78,48 @@ class ORMBaseClass:
 
 class CRUDResource(ORMBaseClass):
     """abstract class for GET and POST method"""
+
+    @swagger.operation(
+        notes='List method of this table',
+        responseMessages=[
+            {
+                "code": 200,
+                "message": "[List of objects]",
+            },
+            {
+                "code": 401,
+                "message": "Missing Authorization Header",
+            },
+            {
+                "code": 403,
+                "message": "Admins only!",
+            },
+        ],
+    )
     def get(self) -> List[Model]:
         return self.make_select_query()
 
+    @swagger.operation(
+        notes='Create method of this table',
+        responseMessages=[
+            {
+                "code": 201,
+                "message": "[Object]",
+            },
+            {
+                "code": 401,
+                "message": "Missing Authorization Header",
+            },
+            {
+                "code": 403,
+                "message": "Admins only!",
+            },
+            {
+                "code": 400,
+                "message": "[Bad Request]",
+            },
+        ],
+    )
     def post(self) -> Tuple[List[Model], int]:
         self.load_parser()
         return [self.make_create_query(**self.args)], 201
@@ -85,12 +130,58 @@ class CRUDResource(ORMBaseClass):
 
 class CRUDRetrieveResource(ORMBaseClass):
     """abstract class for Retrieve, PUT and DELETE method"""
+    @swagger.operation(
+        notes='Get method of this table',
+        responseMessages=[
+            {
+                "code": 200,
+                "message": "[Object]",
+            },
+            {
+                "code": 401,
+                "message": "Missing Authorization Header",
+            },
+            {
+                "code": 403,
+                "message": "Admins only!",
+            },
+            {
+                "code": 404,
+                "message": "[Object] not found",
+            },
+        ],
+    )
     def get(self, instance_id: str) -> Model:
         response = self.make_select_query(id=instance_id)
         if len(response) == 1:
             return response[0]
         abort(404, description=f"{self.table.__name__} not found")  # noqa: R503
 
+    @swagger.operation(
+        notes='Update method of this table',
+        responseMessages=[
+            {
+                "code": 200,
+                "message": "[Object]",
+            },
+            {
+                "code": 401,
+                "message": "Missing Authorization Header",
+            },
+            {
+                "code": 403,
+                "message": "Admins only!",
+            },
+            {
+                "code": 400,
+                "message": "[Bad Request]",
+            },
+            {
+                "code": 404,
+                "message": "[Object] not found",
+            },
+        ],
+    )
     def put(self, instance_id) -> Type[Model]:
         instance = self.make_select_query(id=instance_id)
         if not instance:
@@ -98,6 +189,27 @@ class CRUDRetrieveResource(ORMBaseClass):
         self.load_parser()
         return self.make_update_query(instance[0], self.args)
 
+    @swagger.operation(
+        notes='Delete method of this table',
+        responseMessages=[
+            {
+                "code": 204,
+                "message": "[Object] was deleted",
+            },
+            {
+                "code": 401,
+                "message": "Missing Authorization Header",
+            },
+            {
+                "code": 403,
+                "message": "Admins only!",
+            },
+            {
+                "code": 404,
+                "message": "[Object] not found",
+            },
+        ],
+    )
     def delete(self, instance_id: int) -> Tuple[str, int]:
         output = self.make_delete_query(instance_id)
         if not output:
